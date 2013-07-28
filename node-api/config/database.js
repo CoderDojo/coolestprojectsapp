071b8cf3ca1ccf9bf640e6585d4e3ae6e2001936
Ben Chapman <bchapman@engineyard.com>
2013-07-28 13:54:50 +0100

    Change env vars for hosting

--- node-api/config/database.js
+++ node-api/config/database.js
@@ -1,8 +1,8 @@
 var databaseConfig = { 
-    user: process.env['MYSQL_USER'] || "root",
-    password: process.env['MYSQL_PASS'] || "",
-    database: process.env['MYSQL_NAME'] || "coolestprojects",
-    host: process.env['MYSQL_HOST'] || "localhost",
+    user: process.env['DB_USER'] || "root",
+    password: process.env['DB_PASS'] || "",
+    database: process.env['DB_NAME'] || "coolestprojects",
+    host: process.env['DB_HOST'] || "localhost",
     port: 3306
 };
 
